01870aa0b6df84af192d21c159c517cc04192ebb
Asma Ali <128274795+AsmaAl12@users.noreply.github.com>
2023-04-29 20:34:11 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+// chapter VARIABLES FOR STRINGS //
+
+// A variable can have number of values but only one at a time//
+
+//CamelCase:
+// var myNationality = "Pakistani";
+// myNationality = "Indian";
+
+// var myGender = "Female";
+// myGender = "Male";
+
+// var myVillage = "Jacobabad";
+// myVillage = "Karachi";
+
+
+//Snake case:
+//1.Underscore_   2.Dollar$:
+// var my$school = "Filza Public School";
+// my$school = "Falcon Public School";
+
+// var my_school = "Government Girls High School";
+// my_school = "Santjhon";
+
+// var my$_name = "Ayesha"
+// my$_name = "Asma"
+
+
+
+// You can declare a variable in one statement, leaving it undefined. Then you can assign a
+//value to it in a later statement, without declaring it again.
+
+// var myName;
+// myName = "someone";
+// myName = "Angel";
+
+// var my_country;
+// my_country = "Pakistan";
+
+// var hhhh = "orange";
+// hhhh = "blue";
+
+// var statement = "Thanks for your input";
+// alert (statement);
+
+// var greetings = "hellow n welcome";
+// alert (greetings);
+
+// var welcome = "hellow";
+// alert (welcome);
+
+
+
